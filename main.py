5b416dacc93d1fa48fe0a1c7d0ffe5577c657fd0
KevinAp-5 <keven.clash3@gmail.com>
2023-05-02 01:59:06 -0300

    imports

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import json
+from contextlib import suppress
+from time import sleep
+
+try:
+    from reprint import output
+except ModuleNotFoundError:
+    from subprocess import check_call
+    from sys import executable
+    check_call([executable, '-m', 'pip', 'install', 'reprint'])
+    os.system('clear')
+finally:
+    from reprint import output
+
